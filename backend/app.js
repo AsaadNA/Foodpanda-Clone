4186f3923c3e26c427e8cf5dcab1131e2e596e2d
Asaad Abbasi <asaad.abbasi@gmail.com>
2022-05-28 13:35:08 +0500

    MongoCloud

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(
